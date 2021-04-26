2e337cd5c08f5b710df25886c9ee25db94c6e4a9
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2021-04-26 21:22:59 +0800

    :recycle: introduce period

--- src/main/java/com/odde/budget/BudgetModel.java
+++ src/main/java/com/odde/budget/BudgetModel.java
@@ -2,10 +2,10 @@ package com.odde.budget;
 
 
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.YearMonth;
 import java.util.List;
 
+import static java.time.Period.between;
 import static java.time.temporal.ChronoUnit.MONTHS;
 
 public class BudgetModel {
@@ -21,28 +21,13 @@ public class BudgetModel {
             return 0;
         }
 
-        long dMonth = MONTHS.between(YearMonth.from(start), YearMonth.from(end));
-
-        if (dMonth <= 0) {
-            return getBudgetByMonth(start, end);
-        }
-
-        int sum = getBudgetByMonth(start, YearMonth.from(start).atEndOfMonth());
-
-        for (int i = 0; i < dMonth - 1; i++) {
-            LocalDate temp = start.plusMonths(i + 1);
-            sum += getBudgetByMonth(YearMonth.from(temp).atDay(1), YearMonth.from(temp).atEndOfMonth());
-        }
-
-        sum += getBudgetByMonth(YearMonth.from(end).atDay(1), end);
-
-        return sum;
+        return queryBudgetInPeriod(new Period(start, end));
     }
 
     private int getBudgetByMonth(LocalDate startDate, LocalDate endDate) {
         for (Budget budget : getBudgets()) {
             if (YearMonth.from(startDate).equals(budget.getYearMonth())) {
-                return (Period.between(startDate, endDate).getDays() + 1) * budget.getDailyAmount();
+                return (between(startDate, endDate).getDays() + 1) * budget.getDailyAmount();
             }
         }
 
@@ -51,5 +36,24 @@ public class BudgetModel {
 
     private List<Budget> getBudgets() {
         return repository.findAll();
+    }
+
+    private int queryBudgetInPeriod(Period period) {
+        long dMonth = MONTHS.between(YearMonth.from(period.getStart()), YearMonth.from(period.getEnd()));
+
+        if (dMonth <= 0) {
+            return getBudgetByMonth(period.getStart(), period.getEnd());
+        }
+
+        int sum = getBudgetByMonth(period.getStart(), YearMonth.from(period.getStart()).atEndOfMonth());
+
+        for (int i = 0; i < dMonth - 1; i++) {
+            LocalDate temp = period.getStart().plusMonths(i + 1);
+            sum += getBudgetByMonth(YearMonth.from(temp).atDay(1), YearMonth.from(temp).atEndOfMonth());
+        }
+
+        sum += getBudgetByMonth(YearMonth.from(period.getEnd()).atDay(1), period.getEnd());
+
+        return sum;
     }
 }
---
+++ src/main/java/com/odde/budget/Period.java
@@ -0,0 +1,21 @@
+package com.odde.budget;
+
+import java.time.LocalDate;
+
+public class Period {
+    private final LocalDate start;
+    private final LocalDate end;
+
+    public Period(LocalDate start, LocalDate end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public LocalDate getStart() {
+        return start;
+    }
+
+    public LocalDate getEnd() {
+        return end;
+    }
+}
